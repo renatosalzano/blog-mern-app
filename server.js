747dc76a8356065e446d98dbc43cd3ec4a7972e4
Renato Salzano <renato.salzano92@gmail.com>
2021-06-15 11:13:46 +0200

    fix server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
@@ -31,6 +32,10 @@ mongoose.connect(
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 5000;
