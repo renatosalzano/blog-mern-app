879fa3f9e29b2dbc170ad48d7c9486f0f664201c
Renato Salzano <renato.salzano92@gmail.com>
2021-06-15 11:35:04 +0200

    fix

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ const path = require("path");
 
 const app = express();
 
-app.listen(port, () => console.log(`server in ascolto sulla porta ${port}`));
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use("/auth", require("./router/auth_router"));
@@ -39,3 +32,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server in ascolto sulla porta ${port}`));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
