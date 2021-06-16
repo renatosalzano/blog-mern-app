d5619d5438f9b39b3348329bd43998a2311abb21
Renato Salzano <renato.salzano92@gmail.com>
2021-06-16 10:32:08 +0200

    fix user button

--- client/src/components/user/UserButton.jsx
+++ client/src/components/user/UserButton.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../../auth/AuthContext";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import UserPicture from "./UserPicture";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 
@@ -8,9 +8,17 @@ const UserButton = ({ user }) => {
   const { log_out } = useContext(AuthContext);
   const [menu, setMenu] = useState(false);
   const history = useHistory();
+  const location = useLocation();
+  console.log(location);
 
   const toggle_menu = () => {
     setMenu((state) => !state);
+  };
+
+  const go_to_user_page = () => {
+    if (location.pathname === `/user/${user.user_name}`) return setMenu(false);
+    history.push(`/user/${user.user_name}`);
+    return setMenu(false);
   };
   const log_out_fun = () => {
     history.push("/home");
@@ -24,7 +32,7 @@ const UserButton = ({ user }) => {
       {menu && (
         <UserMenu
           user={user}
-          history={history}
+          user_page_event={go_to_user_page}
           log_out_event={log_out_fun}
           trigger_onclick={toggle_menu}
         />
@@ -35,11 +43,16 @@ const UserButton = ({ user }) => {
 
 export default UserButton;
 
-const UserMenu = ({ user, history, log_out_event, trigger_onclick }) => {
+const UserMenu = ({
+  user,
+  user_page_event,
+  log_out_event,
+  trigger_onclick,
+}) => {
   return (
     <>
       <ul className="user_menu">
-        <li onClick={() => history.push(`/user/${user.user_name}`)}>
+        <li onClick={user_page_event}>
           <UserPicture src={user.pic} is_link={false} />
           <span>{user.user_name}</span>
         </li>
