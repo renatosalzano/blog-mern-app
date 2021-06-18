a65677dfe6d12e553af31b42eea4fdd0628fa256
Renato Salzano <renato.salzano92@gmail.com>
2021-06-18 12:22:10 +0200

    redirect to user page if logged

--- client/src/components/pages/LoginPage.jsx
+++ client/src/components/pages/LoginPage.jsx
@@ -1,14 +1,23 @@
 import { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { AuthContext } from "../../auth/AuthContext";
 import LogIn from "../login/LogIn";
 import SignUp from "../login/SignUp";
 
 const LogInPage = () => {
-  const { signUp, setSignUp, clear_error_message } = useContext(AuthContext);
+  const { signUp, setSignUp, clear_error_message, user } =
+    useContext(AuthContext);
+  const history = useHistory();
 
   useEffect(() => {
     clear_error_message();
   }, [signUp, clear_error_message]);
+
+  useEffect(() => {
+    if (user !== undefined) {
+      history.push(`/user/${user.user_name}`);
+    }
+  });
 
   return (
     <div className="page">
